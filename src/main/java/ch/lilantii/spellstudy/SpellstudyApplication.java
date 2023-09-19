25463aaeff74a6529ce4de16ba25516669a58c64
e536317 <antonia.furrer@sbb.ch>
2023-09-19 21:07:56 +0200

    init

---
+++ src/main/java/ch/lilantii/spellstudy/SpellstudyApplication.java
@@ -0,0 +1,13 @@
+package ch.lilantii.spellstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpellstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/lilantii/spellstudy/SpellstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.lilantii.spellstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpellstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
